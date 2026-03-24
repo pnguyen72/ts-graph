7f1299f89a03a9410417682dbf6d18c129f0f2dc
Felix <pnguyen72@my.bcit.ca>
2026-03-24 15:25:56 -0700

    remove unreachable path, small refactor

--- lib/dijkstra.d.ts
+++ lib/dijkstra.d.ts
@@ -35,29 +35,28 @@ type search<
 	current extends Node,
 	visited extends NodeTable,
 	unvisited extends NodeTable,
-	newVisited extends NodeTable = NodeTable.add<current, visited>,
+	nextVisited extends NodeTable = NodeTable.add<current, visited>,
 > =
 	Fn.pipe<
 		graph,
 		[
 			Graph.neighbors<current["name"]>,
-			List.filterMap<updateNeighbor<current, unvisited>>,
+			List.filterMap<relax<unvisited, current>>,
 			List.foldLeft<NodeTable.update, unvisited>,
 			NodeTable.removeMin,
 		]
-	> extends [infer next extends Node, infer newUnvisited extends NodeTable]
-		? Node.relaxed<next> extends false
+	> extends [infer next extends Node, infer nextUnvisited extends NodeTable]
+		? Node.isRelaxed<next> extends false
 			? `There's no path from ${src} to ${des}`
 			: next["name"] extends des
-				? Fn.call<Node.getPath, next>
-				: search<src, des, graph, next, newVisited, newUnvisited>
-		: Fn.pipe<newVisited, [NodeTable.get<des>, Node.getPath]>;
+				? Node.resolve<next>
+				: search<src, des, graph, next, nextVisited, nextUnvisited>
+		: unknown; // should be unreachable
 
-interface updateNeighbor<current extends Node, unvisited extends NodeTable>
+interface relax<unvisited extends NodeTable, current extends Node>
 	extends Fn<Edge> {
 	return: this["arg"] extends Edge.of<infer _, infer name, infer edgeLength>
-		? Fn.call<NodeTable.get<name>, unvisited> extends infer neighbor extends
-				Node
+		? NodeTable.get<name, unvisited> extends infer neighbor extends Node
 			? plus<current["dist"], edgeLength> extends infer newDist extends number
 				? lt<newDist, neighbor["dist"]> extends true
 					? Node.of<name, newDist, current>
@@ -83,8 +82,14 @@ declare namespace Node {
 		return: lt<this["arg"][0]["dist"], this["arg"][1]["dist"]>;
 	}
 
-	export type relaxed<node extends Node> = lt<node["dist"], inf>;
+	export type isRelaxed<node extends Node> = node["dist"] extends inf
+		? false
+		: true;
 
+	export type resolve<node extends Node> = {
+		path: buildPath<node>;
+		dist: node["dist"];
+	};
 	type buildPath<node extends Node, acc extends string = ""> =
 		node extends of<infer name, infer _, infer prev>
 			? (acc extends "" ? name : `${name} -> ${acc}`) extends infer acc extends
@@ -94,12 +99,6 @@ declare namespace Node {
 					: acc
 				: never
 			: never;
-
-	export interface getPath extends Fn<Node> {
-		return: this["arg"] extends infer node extends Node
-			? { path: buildPath<node>; dist: node["dist"] }
-			: never;
-	}
 }
 
 type NodeTable = Table;
@@ -120,11 +119,8 @@ declare namespace NodeTable {
 		return: Table.remove<node["name"], this["arg"]>;
 	}
 
-	export interface get<name extends string> extends Fn<NodeTable> {
-		return: Table.get<name, this["arg"]> extends infer node extends Node
-			? node
-			: unknown;
-	}
+	export type get<name extends string, table extends NodeTable> =
+		Table.get<name, table> extends infer node extends Node ? node : unknown;
 
 	export type ofList = List.foldLeft<update, empty>;
 
